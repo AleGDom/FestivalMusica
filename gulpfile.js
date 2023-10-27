342c508c6c0608aff5f6a33c07cb609e6b4db8db
Alexis Dominguez <alexis2033ag@outlook.com>
2023-10-27 12:56:23 -0600

    Avance de la seccion del header, hero y sobre festival, se han aligerado imagenes con webp e imaginemin

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,11 @@ const { src, watch, dest } = require("gulp");
 const plumber = require("gulp-plumber");
 const sass = require('gulp-sass')(require('sass'));
 
+//imagenes
+
+const webp = require("gulp-webp");
+
+
 
 function css(done){
     
@@ -15,10 +20,22 @@ function css(done){
     done(); //callback
 }
 
+function towebp(done){
+
+    const opciones={
+        quality :50
+    };
+    src('src/img/**/*.{png,jpg}')
+    .pipe(webp(opciones))
+    .pipe(dest('build/img'));
+    done();
+}
+
 function dev(done){
     watch("src/scss/**/*.scss", css);
     done();
 }
 
 exports.css=css;
+exports.towebp=towebp;
 exports.dev=dev;
