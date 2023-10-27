75ef7f70d4a276775c2f06c7d474454ccd29e4b8
Alexis Dominguez <alexis2033ag@outlook.com>
2023-10-27 17:16:37 -0600

    finalizacion de la seccion del line-up y comienzo del script para la galeria

--- gulpfile.js
+++ gulpfile.js
@@ -2,9 +2,12 @@ const { src, watch, dest } = require("gulp");
 const plumber = require("gulp-plumber");
 const sass = require('gulp-sass')(require('sass'));
 
-//imagenes
 
+//imagenes
 const webp = require("gulp-webp");
+const imagemin = require("gulp-imagemin");
+const cache = require("gulp-cache");
+const avif=require("gulp-avif");
 
 
 
@@ -30,12 +33,42 @@ function towebp(done){
     .pipe(dest('build/img'));
     done();
 }
+function toavif(done){
+
+    const opciones={
+        quality :50
+    };
+    src('src/img/**/*.{png,jpg}')
+    .pipe(avif(opciones))
+    .pipe(dest('build/img'));
+    done();
+}
+
+function imagenes(done){
+    const opciones={
+        optimizationLevel: 3
+    };
+
+    src('src/img/**/*.{png,jpg}')
+    .pipe(cache(imagemin(opciones)))
+    .pipe(dest('build/img'))
+    done();
+}
+function js(done){
+    src('src/js/**/*.js')
+    .pipe(dest('build/js'));
+    done();
+}
 
 function dev(done){
     watch("src/scss/**/*.scss", css);
+    watch("src/js/**/*.js", js);
     done();
 }
 
 exports.css=css;
 exports.towebp=towebp;
 exports.dev=dev;
+exports.imagenes=imagenes;
+exports.toavif=toavif;
+exports.js=js;
