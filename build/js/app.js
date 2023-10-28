63573da9ebd561016c42b590f6aebadbd03ebc5d
Alexis Dominguez <alexis2033ag@outlook.com>
2023-10-27 19:16:19 -0600

    Finalizacion de la seccion de boletos e implementacion del scroll smooth

--- build/js/app.js
+++ build/js/app.js
@@ -19,15 +19,52 @@ function crearGaleria(){
         console.log(imagen);
 
         imagen.addEventListener('click',e=>{
-            mostrarImagen();
+            mostrarImagen(i);
         })
         galeria.appendChild(imagen);   
     }
 }
 
-function mostrarImagen(){
-    console.log('prueba');
+function mostrarImagen(id){
+    const imagen = document.createElement('picture');
+    imagen.innerHTML=`
+        <source srcset="build/img/grande/${id}.avif" type="image/avif">
+        <source srcset="build/img/grande/${id}.webp" type="image/webp">
+        <img loading="lazy" width="200" height="300" src="build/img/grande/${id}.jpg" alt="imagen galeria ${id}">
+    `;
+
+    const overlay = document.createElement('div');
+    overlay.appendChild(imagen);
+    overlay.classList.add('overlay');
+    overlay.addEventListener('click',()=>{
+        overlay.remove();
+        const body = document.querySelector('body');
+        body.classList.remove('fijar-body');
+    })
+    const cerrarModal = document.createElement('p');
+    cerrarModal.textContent='X';
+    cerrarModal.classList.add('btn-cerrar')
+    cerrarModal.addEventListener('click',function(){
+        overlay.remove();
+        const body = document.querySelector('body');
+        body.classList.remove('fijar-body');
+    })
+    overlay.appendChild(cerrarModal);
+    const body = document.querySelector('body');
+    body.classList.add('fijar-body');
+    body.appendChild(overlay);
 }
+
+const enlaces = document.querySelectorAll('.navegacion-principal a');
+
+enlaces.forEach(enlace=>{
+    enlace.addEventListener('click',e=>{
+        e.preventDefault();
+        const seccion=e.target.attributes.href.value;
+        const selector = document.querySelector(seccion);
+        selector.scrollIntoView({behavior:'smooth'});
+    })
+})
 
 
 
